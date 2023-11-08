1c07ed7bfc5fb5f0c506703140c7ef138e79cf3f
hojunlee <hongpc0099@gmail.com>
2023-11-08 22:42:36 +0900

    add insert, select

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ db = mongo_connect["WorkLog"]
 collection = db["Info"]
 
 
-app = Flask(__name)
+app = Flask(__name__)
 
 Corpus_File = "chat.txt"
 
@@ -57,15 +57,37 @@ def insert_Info():
     try:
         data = request.get_json()
         if data:
-            collection.insert_one(data)
+            # 중복 데이터 확인: name, age, address 모두 동일한 경우 중복으로 처리
+            existing_data = collection.find_one({"name": data["name"], "age": data["age"], "address": data["address"]})
+            if existing_data:
+                print("중복 데이터를 입력하였습니다.")
+                return jsonify({"message": "이미 저장된 데이터 입니다."}), 400
+            else:
+                collection.insert_one(data)
+                return jsonify({"message": "데이터가 성공적으로 MongoDB에 저장되었습니다."}), 201
         else:
+            print("JSON 형식 데이터가 제공되지 않았습니다.")
             return jsonify({"message": "유효한 JSON 데이터가 제공되지 않았습니다."}), 400
     except Exception as e:
         return jsonify({"message": f"데이터를 MongoDB에 저장하는 중 오류 발생: {e}"}), 500
     
 
-
-
-
+@app.route('/search', methods=['POST'])
+def search_Info():
+    try:
+        data = request.get_json()
+        result = collection.find_one({"name": data["name"], "age": data["age"], "address": data["address"]})
+        
+        if result:
+            # ObjectId를 문자열로 변환
+            result['_id'] = str(result['_id'])
+            # result.pop('_id', None)
+            return jsonify(result)
+        else:
+            return jsonify({"message": "해당 데이터가 존재하지 않습니다."}), 400
+            
+            
+    except Exception as e:
+        return jsonify({"message": f"데이터를 MongoDB에 읽어오던 중 오류 발생: {e}"}), 500
 if __name__ == '__main__':
     app.run(debug=True)
