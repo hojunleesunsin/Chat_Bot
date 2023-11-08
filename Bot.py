f33977d16c07bea0b0b8ace0549d40e29884adf7
kdk0411 <99461483+kdk0411@users.noreply.github.com>
2023-11-08 19:01:23 +0900

    Chat_Bot Study Code Commit&Push

--- Bot.py
+++ Bot.py
@@ -1,35 +1,96 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
-from cleaner import clean_corpus
-from Main_Text_Classification_Model import text_classifier as tc
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+nltk.download('punkt')
+stemmer = LancasterStemmer()
 
-Corpus_File = "chat.txt"
+import numpy as np
+import random
+import json
+from tensorflow import keras
 
-text = "6월 34일, 서울시 강남구 서초동 30-5, 금영 주식회사, 36,000원."
-sub = "기타 내용."
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
 
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+    return np.array(bag)
+file_path = '/content/drive/MyDrive/intents.json'
+with open(file_path) as file:
+    data = json.load(file)
 
-chatbot = ChatBot("chatpot")
+words = []
+labels = []
+docs_x = []
+docs_y = []
 
-trainer = ListTrainer(chatbot)
-# trainer = ([
-#     "Hi",
-#     "Welcome, My friend",
-# ])
-# trainer.train([
-#     "Are you Plant?",
-#     "No, I'm the pot below the plant",
-# ])
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
 
-clean_corpus = clean_corpus(Corpus_File)
-trainer.train(clean_corpus)
-# 프로젝트 주요 텍스트 분류 코드
-trainer.train(tc(text, sub))
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
+
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+out_empty = [0 for _ in range(len(labels))]
+for x, doc in enumerate(docs_x):
+    bag = []
+    wrds = [stemmer.stem(w.lower())for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = np.array(training)
+output = np.array(output)
+
+model = keras.Sequential([
+    keras.layers.Input(shape=(len(training[0]),)),
+    keras.layers.Dense(8, activation='relu'),
+    keras.layers.Dense(8, activation='relu'),
+    keras.layers.Dense(len(output[0]), activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(training, output, epochs=1500, batch_size=16, verbose=1)
+
+model.save("model_keras.h5")
 
 exit_conditions = ("q", "quit", "exit")
-while True:
-    query = input("> ")
-    if query in exit_conditions:
-        break
-    else:
-        print(f"{chatbot.get_response(query)}")
+def chat():
+    print("Start talking with your bot (type quit to stop)!")
+    while True:
+        q = input(">: ")
+        if q in exit_conditions:
+            break
+
+        ret = model.predict(np.array([bag_of_words(q, words)]))
+        ret_index = np.argmax(ret)
+        tag = labels[ret_index]
+
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+
+        print(random.choice(responses))
+
+chat()
