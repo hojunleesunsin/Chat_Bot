0246af5dc4033099c959745206699e5535a3445a
kdk0411 <wnsldjqja@gmail.com>
2023-11-09 23:17:31 +0900

    전처리, 모델링 과정 클래스, 함수로 분리 ChatBot에 텍스트 분류기 포함시킴

--- Bot.py
+++ Bot.py
@@ -1,81 +1,20 @@
+import numpy as np
+import random
+from tensorflow import keras
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 nltk.download('punkt')
 stemmer = LancasterStemmer()
+from Data_PreProcess import Data_PreProcess
+from bag_of_words import bag_of_words
+from Main_Text_Classification_Model import Text_Classification
 
-import numpy as np
-import random
-import json
-from tensorflow import keras
+words, labels, data = Data_PreProcess()
 
-def bag_of_words(s, words):
-    bag = [0 for _ in range(len(words))]
-    s_words = nltk.word_tokenize(s)
-    s_words = [stemmer.stem(word.lower()) for word in s_words]
-
-    for se in s_words:
-        for i, w in enumerate(words):
-            if w == se:
-                bag[i] = 1
-    return np.array(bag)
-file_path = '/content/drive/MyDrive/intents.json'
-with open(file_path) as file:
-    data = json.load(file)
-
-words = []
-labels = []
-docs_x = []
-docs_y = []
-
-for intent in data['intents']:
-    for pattern in intent['patterns']:
-        wrds = nltk.word_tokenize(pattern)
-        words.extend(wrds)
-        docs_x.append(wrds)
-        docs_y.append(intent["tag"])
-
-    if intent['tag'] not in labels:
-        labels.append(intent['tag'])
-
-words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-words = sorted(list(set(words)))
-
-labels = sorted(labels)
-
-training = []
-output = []
-out_empty = [0 for _ in range(len(labels))]
-for x, doc in enumerate(docs_x):
-    bag = []
-    wrds = [stemmer.stem(w.lower())for w in doc]
-
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
-
-    training.append(bag)
-    output.append(output_row)
-
-training = np.array(training)
-output = np.array(output)
-
-model = keras.Sequential([
-    keras.layers.Input(shape=(len(training[0]),)),
-    keras.layers.Dense(8, activation='relu'),
-    keras.layers.Dense(8, activation='relu'),
-    keras.layers.Dense(len(output[0]), activation='softmax')
-])
-
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(training, output, epochs=1500, batch_size=16, verbose=1)
-
-model.save("Chat_Bot.h5")
+model = keras.models.load_model("Chat_Bot.h5")
 
 exit_conditions = ("q", "quit", "exit")
+
 def chat():
     print("Start talking with your bot (type quit to stop)!")
     while True:
@@ -87,10 +26,36 @@ def chat():
         ret_index = np.argmax(ret)
         tag = labels[ret_index]
 
+        responses = []
         for tg in data["intents"]:
             if tg['tag'] == tag:
-                responses = tg['responses']
+                responses = tg.get('responses', [])
+        if not responses:
+            print("I'm Sorry, I dont understand")
+        else:
+            print(random.choice(responses))
+    # 이거 DB에 저장하면 초기화 시켜줘야함
+    Update_Data = {
+        "time": None,
+        "date": None,
+        "address": None,
+        "pay": None
+    }
+    classifier = Text_Classification(q, None)
+    time = classifier.Time_Pattern()
+    date = classifier.Date_Pattern()
+    address = classifier.Address_Pattern()
+    pay = classifier.Pay_Pattern()
 
-        print(random.choice(responses))
+    if time is not None:
+        Update_Data["time"] = time
+    if date is not None:
+        Update_Data["date"] = date
+    if address is not None:
+        Update_Data["address"] = address
+    if pay is not None:
+        Update_Data["pay"] = pay
+    # 이거는 어디다 둬야할지 고민중임
+    print("Your request Data is it?:", Update_Data)
 
 chat()
---
+++ ChatBot_Model_Train.py
@@ -0,0 +1,24 @@
+import numpy as np
+from tensorflow import keras
+from Data_PreProcess import Data_PreProcess
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+nltk.download('punkt')
+stemmer = LancasterStemmer()
+
+training, output = Data_PreProcess()
+
+training = np.array(training)
+output = np.array(output)
+
+model = keras.Sequential([
+    keras.layers.Input(shape=(len(training[0]),)),
+    keras.layers.Dense(8, activation='relu'),
+    keras.layers.Dense(8, activation='relu'),
+    keras.layers.Dense(len(output[0]), activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(training, output, epochs=1500, batch_size=16, verbose=1)
+
+model.save("Chat_Bot.h5")
